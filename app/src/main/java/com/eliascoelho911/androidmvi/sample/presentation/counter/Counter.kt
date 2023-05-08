211c6f220c6683cf57e8e5aa44e1856b752ac6fb
elias <elias_coelho911@hotmail.com>
2023-05-08 18:06:26 -0300

    added: Adiciona sideEffect

--- app/src/main/java/com/eliascoelho911/androidmvi/sample/presentation/counter/Counter.kt
+++ app/src/main/java/com/eliascoelho911/androidmvi/sample/presentation/counter/Counter.kt
@@ -1,5 +1,6 @@
 package com.eliascoelho911.androidmvi.sample.presentation.counter
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,10 +15,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.eliascoelho911.androidmvi.sample.R
@@ -26,6 +29,19 @@ import org.koin.androidx.compose.koinViewModel
 @Composable
 fun CounterScreen(viewModel: CounterViewModel = koinViewModel()) {
     val state by viewModel.state.collectAsState()
+    val context = LocalContext.current
+
+    LaunchedEffect(viewModel.sideEffect) {
+        viewModel.sideEffect.collect {
+            when (it) {
+                is CounterSideEffect.ShowToast -> Toast.makeText(
+                    context,
+                    it.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
 
     CounterContent(
         state = state,
--- app/src/main/java/com/eliascoelho911/androidmvi/sample/presentation/counter/CounterViewModel.kt
+++ app/src/main/java/com/eliascoelho911/androidmvi/sample/presentation/counter/CounterViewModel.kt
@@ -31,6 +31,7 @@ class CounterViewModel(
 
     private suspend fun onIncrement() {
         on(guard = { it is CounterSyncState.Loaded }) {
+            sendSideEffect(CounterSideEffect.ShowToast("Incrementing..."))
             repository.increment().onSuccess { counter ->
                 reduce { it.loaded(counter) }
             }.onFailure { throwable ->
@@ -41,6 +42,7 @@ class CounterViewModel(
 
     private suspend fun onDecrement() {
         on(guard = { it is CounterSyncState.Loaded }) {
+            sendSideEffect(CounterSideEffect.ShowToast("Decrementing..."))
             repository.decrement().onSuccess { counter ->
                 reduce { it.loaded(counter) }
             }.onFailure { throwable ->
